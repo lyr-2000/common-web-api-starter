ae4f7a936c22313fff14ad623c854980cbbde423
idea-your-father <1957476540@qq.com>
2021-02-04 23:28:05 +0800

    添加pageInfo类

---
+++ src/main/java/com/github/lyr2000/common/dto/PageInfo.java
@@ -0,0 +1,34 @@
+package com.github.lyr2000.common.dto;
+
+import com.github.pagehelper.Page;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * @Author lyr
+ * @create 2021/2/4 23:26
+ */
+@Data
+@Accessors(chain = true)
+public class PageInfo<T> {
+
+    private Integer totalPage;
+    private Integer curPage;
+    private Integer size;
+    private Long totalCount;
+
+    private List<T> list;
+
+    public static <T> PageInfo<T> of(Page<T> ipage) {
+        PageInfo<T> pageInfo = new PageInfo<>();
+        pageInfo.curPage = ipage.getPageNum();
+        pageInfo.size = ipage.getPageSize();
+        pageInfo.totalCount = ipage.getTotal();
+        pageInfo.list = ipage.getResult();
+        pageInfo.totalPage = ipage.getPages();
+        return pageInfo;
+    }
+
+}
